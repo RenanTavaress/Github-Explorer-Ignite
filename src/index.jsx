d3acf918e895cb14f9a06e7d129b794bca221e62
Renan Tavares <renan_velox@hotmail.com>
2021-10-05 17:55:30 -0300

    Configurando o webpack, criando o componente App

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
+import { App } from './App'
+import { render } from 'react-dom'
 
-function App(){
-   return <h1>Hello world</h1>
-}
+render(<App/>, document.getElementById('root'))
